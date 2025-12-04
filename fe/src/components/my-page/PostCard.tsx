4b1c08a0b688c167d7f606a54795fbd888b2aae4
HaeunJang271 <haeunjang271@gmail.com>
2025-12-04 19:27:50 +0900

    fix: 게시글 카드 높이 달라짐 해결

--- fe/src/components/my-page/PostCard.tsx
+++ fe/src/components/my-page/PostCard.tsx
@@ -66,7 +66,7 @@ const PostCard = ({
       {/* 게시글 정보 */}
       <div className="flex h-27 flex-col justify-between gap-1 p-2">
         {/* 제목/태그 */}
-        <Typography font="noto" variant="label2M" className="text-pink-600">
+        <Typography font="noto" variant="label2M" className="truncate text-pink-600">
           {title}
         </Typography>
 
