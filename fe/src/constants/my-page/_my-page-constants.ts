34fea1f60870dc559ee88583460f44ed51e3a41b
grapefruit <liam.heo11@gmail.com>
2026-01-18 14:42:04 +0900

    feat: 마이페이지-한끗루틴현황 캘린더 구현 및 바뀐 디자인 적용

---
+++ fe/src/constants/my-page/_my-page-constants.ts
@@ -0,0 +1,9 @@
+export const WEEK_DAYS = ["일", "월", "화", "수", "목", "금", "토"] as const;
+
+/** 달력 관련 상수 */
+export const CALENDAR_CONSTANTS = {
+  /** 첫 번째 월 */
+  FIRST_MONTH: 1,
+  /** 마지막 월 */
+  LAST_MONTH: 12,
+} as const;
